fa8481645337ed6a8f4a129d5f70bfd20d49d1c2
Gianluca Geusa <geuluk@gmail.com>
2021-04-29 08:37:01 +0200

    Update server.js

--- server.js
+++ server.js
@@ -2,6 +2,11 @@ require('dotenv').config();
 const express = require('express');
 const cors = require('cors');
 const app = express();
+const bodyParser = require('body-parser');
+app.use(bodyParser.urlencoded({ extended: true }));
+
+var storage = new Map();
+var counter = 0;
 
 // Basic Configuration
 const port = process.env.PORT || 3000;
@@ -17,6 +22,28 @@ app.get('/', function(req, res) {
 // Your first API endpoint
 app.get('/api/hello', function(req, res) {
   res.json({ greeting: 'hello API' });
+});
+
+//getUrl
+app.get('/api/shorturl/:id?', function(req, res) {
+  var url = storage.get(req.params.id);
+  if (url!=null){
+    res.redirect(url)
+  } else {
+    res.json({ error: 'invalid url' });
+  }
+});
+
+//addUrl
+app.post('/api/shorturl/', function(req, res) {
+  var url = req.body.url;
+  if (/^(?:http(s)?:\/\/)?[\w.-]+(?:\.[\w\.-]+)+[\w\-\._~:/?#[\]@!\$&'\(\)\*\+,;=.]+$/.test(url)){
+    var id = ++counter;
+    storage.set(String(id), url);
+    res.json({ original_url : url, short_url : id});
+  } else {
+    res.json({ error: 'invalid url' });
+  }
 });
 
 app.listen(port, function() {
